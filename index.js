c6390615b07304f3c57bdfb47c168151b15306a5
Dan Laush <dan.laush@gmail.com>
2017-08-13 15:57:38 +1000

    Clear word timeout on stop and reset

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { TextArea } from './components/textArea';
 
 export default class App extends Component {
 	state = {
-		text: "A Town Mouse once visited a relative who lived in the country. For lunch the Country Mouse served wheat stalks, roots, and acorns, with a dash of cold water for drink. The Town Mouse ate very sparingly, nibbling a little of this and a little of that, and by her manner making it very plain that she ate the simple food only to be polite.",
+		text: "BEIJING — In a diplomatic gamble, President Trump is seeking to enlist China as a peacemaker in the bristling nuclear-edged dispute with North Korea at the very moment he plans to ratchet up conflict with Beijing over trade issues that have animated his political rise. Mr. Trump spoke late Friday with his counterpart, President Xi Jinping of China, to press the Chinese to do more to rein in North Korea as it races toward development of long-range nuclear weapons that could reach the United States. Mr. Xi sought to lower the temperature after Mr. Trump’s vow to rain down “fire and fury” on North Korea, urging restraint and a political solution. But the conversation came as Mr. Trump’s administration was preparing new trade action against China that could inflame the relationship. Mr. Trump plans to return to Washington on Monday to sign a memo determining whether China should be investigated for intellectual property violations, accusing Beijing of failing to curb the theft of trade secrets and rampant online and physical piracy and counterfeiting. An investigation would be intended to lead to retaliatory measures. The White House had planned to take action on intellectual property earlier but held off as it successfully lobbied China to vote at the United Nations Security Council for additional sanctions on North Korea a week ago. Even now, the extra step of determining whether to start the investigation is less than trade hawks might have wanted, but softens the blow to China and gives Mr. Trump a cudgel to hold over it if he does not get the cooperation he wants. While past presidents have tried at least ostensibly to keep security and economic issues on separate tracks in their dealings with China, Mr. Trump has explicitly linked the two, suggesting he would back off from a trade war against Beijing if it does more to pressure North Korea. “If China helps us, I feel a lot differently toward trade, a lot differently toward trade,” he told reporters on Thursday.",
 		textNodes: [],
 		activeNode: 0,
 		speed: 0,
@@ -18,6 +18,7 @@ export default class App extends Component {
 
 	constructor() {
 		super();
+		this.timer;
 		this.changeSpeed = throttle(this.changeSpeed.bind(this),100);
 		this.stop = this.stop.bind(this);
 		this.reset = this.reset.bind(this);
@@ -34,11 +35,14 @@ export default class App extends Component {
 		let value = event.target.value,
 		oldSpeed = this.state.speed;
 		this.setState({speed:value});
-		if(oldSpeed == 0) this.updateWord();
+		if(oldSpeed == 0) {
+			clearTimeout(this.timer);
+			this.updateWord();
+		}
 	}
 
 	updateWord() {
-		setTimeout(() => {
+		this.timer = setTimeout(() => {
 			if(this.state.speed<0) {
 				if (this.state.activeNode-1 >= 0)
 					this.setState({ activeNode: this.state.activeNode-1 });
@@ -64,6 +68,7 @@ export default class App extends Component {
 		this.setState({ 
 			speed: 0
 		});
+		clearTimeout(this.timer);
 	}
 
 	reset() {
@@ -71,6 +76,7 @@ export default class App extends Component {
 			activeNode: 0,
 			percentComplete: 0
 		});
+		clearTimeout(this.timer);
 	}
 
 	render(props) {
