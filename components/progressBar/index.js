9f937c05fae54f24b743f8bc8e14a2153e44fc1c
Dan Laush <dan.laush@tundra.com.au>
2017-10-10 17:02:41 +1100

    Add a dot to the progress bar

--- components/progressBar/index.js
+++ components/progressBar/index.js
@@ -1,5 +1,12 @@
 export const ProgressBar = ({ percentComplete }) => (
 	<div class="progress-bar">
-		<div class="progress-bar__current" style={`transform:scaleX(${percentComplete/100})`}><span>{percentComplete}% Complete</span></div>
+		<div class="progress-bar__bar">
+			<div 
+				class="progress-bar__current" 
+				style={`transform:scaleX(${percentComplete/100})`}>
+					<span>{percentComplete}% Complete</span>
+			</div>
+		</div>
+		<div class="progress-bar__drag"></div>
 	</div>
 );
